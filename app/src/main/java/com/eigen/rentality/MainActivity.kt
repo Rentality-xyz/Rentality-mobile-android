5e729949cc768c2930dcf9c3752ce2d8aae424b8
danyloday <danil.vladyka1@gmail.com>
2025-08-19 13:16:59 +0300

    Fixed nav bar

--- app/src/main/java/com/eigen/rentality/MainActivity.kt
+++ app/src/main/java/com/eigen/rentality/MainActivity.kt
@@ -2,27 +2,31 @@ package com.eigen.rentality
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.content.ContextCompat
 import com.eigen.rentality.webview.RentalityView
 
 class MainActivity : ComponentActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        println("tokenecrctvytrtrbt222")
-        println(getSharedPreferences(PUSH_TOKEN,0).getString(PUSH_TOKEN,""))
         setContent {
 
             val requestPermissionLauncher = rememberLauncherForActivityResult(
@@ -41,6 +45,8 @@ class MainActivity : ComponentActivity() {
             Box(
                 modifier = Modifier
                     .statusBarsPadding()
+                    .background(Color(0xFF1E1E30))
+                    .navigationBarsPadding()
                     .fillMaxSize()
             ) {
                 RentalityView()
