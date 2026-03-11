40864c6a9eb2552bbcb66cbc0456d725b559413c
Dima Denisenko <dendima80@gmail.com>
2026-03-11 17:46:12 +0200

    to PROD ver = 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "xyz.rentality.rentality"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.12.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/eigen/rentality/webview/RentalityView.kt
+++ app/src/main/java/com/eigen/rentality/webview/RentalityView.kt
@@ -42,8 +42,8 @@ import coil.size.Size
 import com.eigen.rentality.PUSH_TOKEN
 import com.eigen.rentality.R
 
-//const val RENTALITY_URL = "https://app.rentality.io/"
-const val RENTALITY_URL = "https://demo.rentality.io/"
+const val RENTALITY_URL = "https://app.rentality.io/"
+//const val RENTALITY_URL = "https://demo.rentality.io/"
 //const val RENTALITY_URL = "https://demotest.rentality.io/"
 //const val RENTALITY_URL = "http://localhost:3000/"
 //const val RENTALITY_URL = "https://zora.co/"
